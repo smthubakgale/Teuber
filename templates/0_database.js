6fcb30c5568952e0602769bc6d8bc70643f4d8d1
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2022-06-16 22:31:47 +0200

    Update 0_database.js

--- templates/0_database.js
+++ templates/0_database.js
@@ -1914,7 +1914,7 @@ function testapi()
             "param": $("#api_param").val()
 
         };
-        var a = $("#api_url").val() + '/api/Bridge/GetData';
+        var a = $("#api_url").val() + '/Home/Get0';
 
         var ret = "";
         $.ajax({
